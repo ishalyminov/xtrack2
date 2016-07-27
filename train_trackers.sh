eb0b700f30b66d1d781fb03005ef8f8d22944c47
Igor Shalyminov <ishalyminov@yandex-team.ru>
2016-07-27 13:40:55 +0300

    topic-independent trackers train script added

---
+++ train_trackers.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+for topic in `python -m ontology_util get_topics`; do
+    echo "Training the tracker for '$topic' topic"
+    python xtrack2.py \
+        --mb_size=16 --lr=0.1 --opt_type=rmsprop --p_drop=0.01 --n_cells=100 \
+        --n_epochs=20 --ontology dstc5_scripts/config/ontology_dstc5.json \
+        data/xtrack/e2_tagged_$topic
+done
+
